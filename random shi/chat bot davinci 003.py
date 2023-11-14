7be8fc59efee5a7fb0a54ee3cf68450adb6cc7b1
Fickry Imamsyah <142564782+yozoyaa@users.noreply.github.com>
2023-11-15 01:41:30 +0700

    Add files via upload

---
+++ random shi/chat bot davinci 003.py
@@ -0,0 +1,22 @@
+import openai
+# pip install openai << di cmd sebelum import openai
+
+openai.api_key = "<your API key"
+model_engine = "text-davinci-003"
+
+def chatbot(prompt):
+    completions = openai.Completion.create(
+        engine = model_engine,
+        prompt = prompt,
+        max_tokens = 1024,
+        n = 1,
+        stop = None,
+        temperature = 0.5,
+    )
+    message = completions.choices[0].text
+    return message
+
+while True:
+    prompt = input("you: ")
+    response = chatbot(prompt)
+    print("Chat bot: " + response)
